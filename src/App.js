c3869c7ca94abd274da6bf4dea7067ebce3a8b44
Pedro Gomes <pedro.gomes@poli.ufrj.br>
2021-05-12 00:47:02 -0300

    Completed useEffect part of the tutorial

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import UseStateBasics from './tutorial/1-useState/setup/5-useState-counter'
+import UseEffectBasics from './tutorial/2-useEffect/setup/3-useEffect-fetch-data';
 function App() {
   return (
     <div className='container'>
-      <UseStateBasics></UseStateBasics>
+      <UseEffectBasics></UseEffectBasics>
     </div>
   )
 }
--- src/tutorial/2-useEffect/setup/1-useEffect-basics.js
+++ src/tutorial/2-useEffect/setup/1-useEffect-basics.js
@@ -2,8 +2,21 @@ import React, { useState, useEffect } from 'react';
 // by default runs after every re-render
 // cleanup function
 // second parameter
+// work outside component signing up for subscription etc
 const UseEffectBasics = () => {
-  return <h2>useEffect Basics</h2>;
+  const [value, setValue] = useState(0);
+  useEffect(()=>{
+    if (value > 0){
+    document.title = `New messages(${value})`
+  }else{
+    document.title = 'Normal title'
+  }
+  });
+  return <>
+  <h1>{value}</h1>
+  <button className='btn' onClick={()=>setValue((prevState)=> prevState+1)}>click me</button>
+  <button className='btn' onClick={()=> setValue(0)}>Reset</button>
+  </>;
 };
 
 export default UseEffectBasics;
